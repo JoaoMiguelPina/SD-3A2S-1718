d200b14a18fa1bf7609db7e4fd54ebeed5ed4af3
regouga <miguelregouga@tecnico.ulisboa.pt>
2018-04-11 03:09:26 +0100

    Fixed a couple more things

--- binas-ws-cli/src/main/java/org/binas/ws/cli/BinasClientApp.java
+++ binas-ws-cli/src/main/java/org/binas/ws/cli/BinasClientApp.java
@@ -2,45 +2,43 @@ package org.binas.ws.cli;
 
 public class BinasClientApp {
 
-    public static void main(String[] args) throws Exception {
-        // Check arguments
-        if (args.length == 0) {
-            System.err.println("Argument(s) missing!");
-            System.err.println("Usage: java " + BinasClientApp.class.getName()
-                    + " wsURL OR uddiURL wsName");
-            return;
-        }
-        String uddiURL = null;
-        String wsName = null;
-        String wsURL = null;
-        if (args.length == 1) {
-            wsURL = args[0];
-        } else if (args.length >= 2) {
-            uddiURL = args[0];
-            wsName = args[1];
-        }
+	public static void main(String[] args) throws Exception {
+		// Check arguments.
+		if (args.length == 0) {
+			System.err.println("Argument(s) missing!");
+			System.err.println("Usage: java " + BinasClientApp.class.getName() + " wsURL OR uddiURL wsName");
+			return;
+		}
+		String uddiURL = null;
+		String wsName = null;
+		String wsURL = null;
+		if (args.length == 1) {
+			wsURL = args[0];
+		} else if (args.length >= 2) {
+			uddiURL = args[0];
+			wsName = args[1];
+		}
 
 		System.out.println(BinasClientApp.class.getSimpleName() + " running");
 
-        // Create client
-        BinasClient client = null;
+		// Create client.
+		BinasClient client = null;
 
-        if (wsURL != null) {
-            System.out.printf("Creating client for server at %s%n", wsURL);
-            client = new BinasClient(wsURL);
-        } else if (uddiURL != null) {
-            System.out.printf("Creating client using UDDI at %s for server with name %s%n",
-                uddiURL, wsName);
-            client = new BinasClient(uddiURL, wsName);
-        }
+		if (wsURL != null) {
+			System.out.printf("Creating client for server at %s%n", wsURL);
+			client = new BinasClient(wsURL);
+		} else if (uddiURL != null) {
+			System.out.printf("Creating client using UDDI at %s for server with name %s%n", uddiURL, wsName);
+			client = new BinasClient(uddiURL, wsName);
+		}
 
-        // the following remote invocations are just basic examples
-        // the actual tests are made using JUnit
+		// The following remote invocation is just a basic example.
+		// The actual tests are made using JUnit.
 
-//		 System.out.println("Invoke ping()...");
-//		 String result = client.testPing("client");
-//		 System.out.print(result);
-        
-	 }
+		System.out.println("Invoke ping()...");
+		String result = client.testPing("client");
+		System.out.print("Result: ");
+		System.out.println(result);
+	}
 }
 
