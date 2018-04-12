97b877ab893fc2754ee46bc1549862478fabff36
heliodomingos <helio_domingos@hotmail.com>
2018-04-12 22:33:22 +0100

    closes #20 rentBina test

--- binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaTest.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaTest.java
@@ -1,74 +1,272 @@
 package org.binas.ws.it;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.IOException;
 import java.util.Properties;
 
-import org.binas.station.ws.cli.StationClient;
-import org.binas.station.ws.it.BaseIT;
+import org.binas.ws.it.BaseIT;
 import org.binas.ws.AlreadyHasBina_Exception;
+import org.binas.ws.BadInit_Exception;
+import org.binas.ws.EmailExists_Exception;
+import org.binas.ws.InvalidEmail_Exception;
 import org.binas.ws.InvalidStation_Exception;
 import org.binas.ws.NoBinaAvail_Exception;
 import org.binas.ws.NoCredit_Exception;
 import org.binas.ws.UserNotExists_Exception;
 import org.binas.ws.cli.BinasClient;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class RentBinaTest {
+public class RentBinaTest extends BaseIT{
 
-	private static final String TEST_PROP_FILE = "/test.properties";
-	protected static Properties testProps;
+	String stationId1 = "A46_Station1";
+	String stationId2 = "A46_Station2";
+	String stationId3 = "A46_Station3";
 	
-//	BinasClient
-	protected static BinasClient client;
+	String userMail = "sd.rent@tecnico.pt";
 	
-	/*
-	@Before
-	public void setup() {
-		testProps = new Properties();
+	public void success() {
 		try {
-			testProps.load(RentBinaTest.class.getResourceAsStream(TEST_PROP_FILE));
-			System.out.println("Loaded test properties:");
-			System.out.println(testProps);
-		} catch (IOException e) {
-			final String msg = String.format("Could not load properties file {}", TEST_PROP_FILE);
-			System.out.println(msg);
-			throw e;
-		}
+			
+			//STATION 1
+			int bonus1 = 1;
+			client.testInitStation(stationId1, 10, 10, 5, bonus1);
+			org.binas.ws.StationView sv1 = client.getInfoStation(stationId1);
+			assertNotNull(sv1);
+			
+			client.activateUser(userMail);
+			client.rentBina(sv1.getId(), userMail);
+			
+			assertEquals(1, client.getInfoStation(sv1.getId()).getFreeDocks());
+			assertEquals(1, client.getInfoStation(sv1.getId()).getTotalGets());
+			
+			//STATION 2
+			int bonus2 = 2;
+			client.testInitStation(stationId2, 17, 17, 4, bonus2);
+			org.binas.ws.StationView sv2 = client.getInfoStation(stationId2);
+			assertNotNull(sv2);
+			
+			client.activateUser(userMail);
+			client.rentBina(sv2.getId(), userMail);
+			
+			assertEquals(1, client.getInfoStation(sv2.getId()).getFreeDocks());
+			assertEquals(1, client.getInfoStation(sv2.getId()).getTotalGets());
 
-		final String uddiEnabled = testProps.getProperty("uddi.enabled");
-		final String verboseEnabled = testProps.getProperty("verbose.enabled");
+			
+			//STATION 3
+			int bonus3 = 3;
+			client.testInitStation(stationId3, 7, 7, 22, bonus3);
+			org.binas.ws.StationView sv3 = client.getInfoStation(stationId3);
+			assertNotNull(sv3);
+			
+			client.activateUser(userMail);
+			client.rentBina(sv3.getId(), userMail);
+			
+			assertEquals(1, client.getInfoStation(sv3.getId()).getFreeDocks());
+			assertEquals(1, client.getInfoStation(sv3.getId()).getTotalGets());
+			
+		}catch(BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		}catch (UserNotExists_Exception e) {
+			System.out.println("The user: " + e + "doesnt exists.");
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidStation_Exception e) {
+			System.out.println("The provided station (" + e + ") is invalid.");
+		} catch (AlreadyHasBina_Exception e) {
+			System.out.println("The provided user (" + e + ") already had rent a bina.");
+		} catch (NoBinaAvail_Exception e) {
+			System.out.println("The provided station (" + e + ") doesnt have any bina.");
+		} catch (NoCredit_Exception e) {
+			System.out.println("The provided user (" + e + ") doesnt have credit.");
+		} 
 
-		final String uddiURL = testProps.getProperty("uddi.url");
-		final String wsName = testProps.getProperty("ws.name");
-		final String wsURL = testProps.getProperty("ws.url");
-
-		if ("true".equalsIgnoreCase(uddiEnabled)) {
-			client = new BinasClient(uddiURL, wsName);
-		} else {
-			client = new BinasClient(wsURL);
-		}
-		client.setVerbose("true".equalsIgnoreCase(verboseEnabled));
-		
-	}
-	
-	@Test
-	public void success(){
-		
 	}
 	
 	@Test(expected = AlreadyHasBina_Exception.class)
-	public void AlreadyHasBinaException() {}
+	public void AlreadyHasBinaException() {
+		
+		int bonus1 = 1;
+		try {
+			client.testInitStation(stationId1, 10, 10, 5, bonus1);
+			client.activateUser(userMail);
+			client.rentBina(stationId1, userMail);
+			client.rentBina(stationId1, userMail);
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		} catch (InvalidStation_Exception e) {
+			System.out.println("The provided station (" + e + ") is invalid.");
+		} catch (AlreadyHasBina_Exception e) {
+			System.out.println("The provided user (" + e + ") already had rent a bina.");
+		} catch (NoBinaAvail_Exception e) {
+			System.out.println("The provided station (" + e + ") doesnt have any bina.");
+		} catch (NoCredit_Exception e) {
+			System.out.println("The provided user (" + e + ") doesnt have credit.");
+		} catch (UserNotExists_Exception e) {
+			System.out.println("The user: " + e + "doesnt exists.");
+		}	
+	}
+	
+	@Test(expected = InvalidEmail_Exception.class)
+	public void InvalidEmailException() {
+		
+		try {
+			client.testInitStation(stationId1, 10, 10, 5, 1);
+			client.activateUser("mail_invalido");
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		} 
+	}
+	
+	@Test(expected = EmailExists_Exception.class)
+	public void EmailExistsException() {
+		
+		try {
+			client.testInitStation(stationId1, 10, 10, 5, 1);
+			client.activateUser(userMail);
+			client.activateUser(userMail);
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		}
+	}
+	
+	@Test(expected = BadInit_Exception.class)
+	public void BadInitException() {
+		
+		int bonus1 = 1;
+		try {
+			client.testInitStation(null, 10, 10, 5, bonus1);
+			client.activateUser(userMail);
+			client.rentBina(stationId1, userMail);
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		} catch (InvalidStation_Exception e) {
+			System.out.println("The provided station (" + e + ") is invalid.");
+		} catch (AlreadyHasBina_Exception e) {
+			System.out.println("The provided user (" + e + ") already had rent a bina.");
+		} catch (NoBinaAvail_Exception e) {
+			System.out.println("The provided station (" + e + ") doesnt have any bina.");
+		} catch (NoCredit_Exception e) {
+			System.out.println("The provided user (" + e + ") doesnt have credit.");
+		} catch (UserNotExists_Exception e) {
+			System.out.println("The user: " + e + "doesnt exists.");
+		}	
+	}
 	
 	@Test (expected = InvalidStation_Exception.class)
-	public void InvalidStationException() {}
+	public void InvalidStationException()  {
+		try {
+			client.testInitStation("CXX_Station1", 10, 10, 1, 2);
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		}
+	}
 	
 	@Test(expected = NoBinaAvail_Exception.class)
-	public void NoBinaAvailableException() {}
+	public void NoBinaAvailableException() {
+		try {
+			client.testInitStation(stationId1, 10, 10, 0, 1);
+			org.binas.ws.StationView sv1 = client.getInfoStation(stationId1);
+			client.activateUser(userMail);
+			
+			client.rentBina(stationId1, userMail);
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (InvalidStation_Exception e) {
+			System.out.println("The provided station (" + e + ") is invalid.");
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		} catch (AlreadyHasBina_Exception e) {
+			System.out.println("The provided user (" + e + ") already had rent a bina.");
+		} catch (NoBinaAvail_Exception e) {
+			System.out.println("The provided station (" + e + ") doesnt have any bina.");
+		} catch (NoCredit_Exception e) {
+			System.out.println("The provided user (" + e + ") doesnt have credit.");
+		} catch (UserNotExists_Exception e) {
+			System.out.println("The user: " + e + "doesnt exists.");
+		}
+		
+	}
 	
 	@Test(expected = NoCredit_Exception.class)
-	public void NoCreditException() {}
+	public void NoCreditException() {
+		try {
+			
+			
+			client.testInitStation(stationId1, 10, 10, 5, 2);
+			client.activateUser(userMail);
+			client.testInit(0);
+			client.rentBina(stationId1, userMail);
+			
+			
+		} catch (BadInit_Exception e) {
+			System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (EmailExists_Exception e) {
+			System.out.println("The provided email (" + e + ") already exists.");
+		} catch (InvalidEmail_Exception e) {
+			System.out.println("The provided email (" + e + ") is invalid.");
+		} catch (AlreadyHasBina_Exception e) {
+			System.out.println("The provided user (" + e + ") already had rent a bina.");
+		} catch (InvalidStation_Exception e) {
+			System.out.println("The provided station (" + e + ") is invalid.");
+		} catch (NoBinaAvail_Exception e) {
+			System.out.println("The provided station (" + e + ") doesnt have any bina.");
+		} catch (NoCredit_Exception e) {
+			System.out.println("The provided user (" + e + ") doesnt have credit.");
+		} catch (UserNotExists_Exception e) {
+			System.out.println("The user: " + e + "doesnt exists.");
+		}
+		
+
+	}
 	
 	@Test(expected = UserNotExists_Exception.class)
-	public void UserNotExistsException() {}*/
+	public void UserNotExistsException() {
+		
+		try {
+			//client.testInitStation(stationId1, 10, 10, 5, 2);
+			client.rentBina(stationId1, userMail);
+		//} catch (BadInit_Exception e) {
+			//System.out.println("There was an error while creating station. Check output: " + e);
+		} catch (AlreadyHasBina_Exception e) {
+			System.out.println("The provided user (" + e + ") already had rent a bina.");
+		} catch (InvalidStation_Exception e) {
+			System.out.println("The provided station (" + e + ") is invalid.");
+		} catch (NoBinaAvail_Exception e) {
+			System.out.println("The provided station (" + e + ") doesnt have any bina.");
+		} catch (NoCredit_Exception e) {
+			System.out.println("The provided user (" + e + ") doesnt have credit.");
+		} catch (UserNotExists_Exception e) {
+			System.out.println("The user: " + e + "doesnt exists.");
+		}
+		
+	}
+	
+	@After
+	public void tearDown() {
+		client.testClear();
+	}
 }
