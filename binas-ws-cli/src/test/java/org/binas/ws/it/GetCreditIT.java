460a5304e4e5dfb08a2282bb67850d7d37848419
João Pina <joaomfpina@tecnico.ulisboa.pt>
2018-04-12 23:26:42 +0100

    more getCreditIT tests

--- binas-ws-cli/src/test/java/org/binas/ws/it/GetCreditIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/GetCreditIT.java
@@ -26,7 +26,7 @@ public class GetCreditIT extends BaseIT{
 		assertEquals(10, 10);
 	}
 	
-	/*@Test(expected = InvalidEmail_Exception.class)
+	@Test(expected = InvalidEmail_Exception.class)
 	public void emailDoesNotExist() throws InvalidEmail_Exception{
 		try {
 			client.testInitStation("A46_Station1", 50, 60, 100, 2);
@@ -95,9 +95,7 @@ public class GetCreditIT extends BaseIT{
 			client.testInitStation("A46_Station1", 50, 60, 100, 2);
 			StationView station = client.getInfoStation("A46_Station1");
 			assertNotNull(station);
-			int credit = client.getCredit("miguel.regouga@gmail.com");
-			System.out.println("Credit: " +  credit);
-			System.out.println("Email: " +  client.getEmail("miguel.regouga@gmail.com"));
+			int credit = client.getCredit("pedro.caldeira@gmail.com");
 			assertEquals(10, credit);
 			
 		} catch (BadInit_Exception e) {
@@ -109,33 +107,46 @@ public class GetCreditIT extends BaseIT{
 		}
 	}
 	
-	/*@Test(expected = BadInit_Exception.class)
-	public void testInitStationFails() throws BadInit_Exception {
-		
+	@Test(expected = InvalidEmail_Exception.class)
+	public void emptyEmail() throws InvalidEmail_Exception{
 		try {
-			client.testInitStation("A46_Station1", -10, 60, 100, 2);
-			StationView station;
-			station = client.getInfoStation("A46_Station1");
+			client.testInitStation("A46_Station1", 50, 60, 100, 2);
+			StationView station = client.getInfoStation("A46_Station1");
 			assertNotNull(station);
 			client.activateUser("miguel.regouga@gmail.com");
+			int credit = client.getCredit("");
+			assertEquals(10, credit);
 			
-			int credit = client.getCredit("miguel.regouga@gmail.com");
-			
+		} catch (BadInit_Exception e) {
+			e.printStackTrace();
 		} catch (InvalidStation_Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UserNotExists_Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (EmailExists_Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidEmail_Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-	}*/
+	}
+	
+	@Test(expected = InvalidEmail_Exception.class)
+	public void spaceEmail() throws InvalidEmail_Exception{
+		try {
+			client.testInitStation("A46_Station1", 50, 60, 100, 2);
+			StationView station = client.getInfoStation("A46_Station1");
+			assertNotNull(station);
+			client.activateUser("miguel.regouga@gmail.com");
+			int credit = client.getCredit(" ");
+			assertEquals(10, credit);
+			
+		} catch (BadInit_Exception e) {
+			e.printStackTrace();
+		} catch (InvalidStation_Exception e) {
+			e.printStackTrace();
+		} catch (UserNotExists_Exception e) {
+			e.printStackTrace();
+		} catch (EmailExists_Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
