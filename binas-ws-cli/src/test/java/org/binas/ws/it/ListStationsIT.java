b0b0e937bf5d3bf6697c2950af29115ff03e7ecb
regouga <miguelregouga@tecnico.ulisboa.pt>
2018-04-13 21:46:11 +0100

    Closes #18 :100:

--- binas-ws-cli/src/test/java/org/binas/ws/it/ListStationsIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/ListStationsIT.java
@@ -1,5 +1,144 @@
 package org.binas.ws.it;
 
-public class ListStationsIT {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.util.List;
+
+import org.binas.ws.AlreadyHasBina_Exception;
+import org.binas.ws.BadInit_Exception;
+import org.binas.ws.CoordinatesView;
+import org.binas.ws.EmailExists_Exception;
+import org.binas.ws.FullStation_Exception;
+import org.binas.ws.InvalidEmail_Exception;
+import org.binas.ws.InvalidStation_Exception;
+import org.binas.ws.NoBinaAvail_Exception;
+import org.binas.ws.NoBinaRented_Exception;
+import org.binas.ws.NoCredit_Exception;
+import org.binas.ws.StationView;
+import org.binas.ws.UserNotExists_Exception;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ListStationsIT extends BaseIT {
+
+	String stationId1 = "A46_Station1";
+	String stationId2 = "A46_Station2";
+	String stationId3 = "A46_Station3";
+
+	String userMail1 = "sd1@tecnico.pt";
+	String userMail2 = "sd2@tecnico.pt";
+	String userMail3 = "sd3@tecnico.pt";
+	
+	StationView sv1;
+	StationView sv2;
+	StationView sv3;
+
+	@Before
+	public void setup() throws BadInit_Exception, EmailExists_Exception, InvalidEmail_Exception,
+			InvalidStation_Exception, UserNotExists_Exception, FullStation_Exception, NoBinaRented_Exception,
+			AlreadyHasBina_Exception, NoBinaAvail_Exception, NoCredit_Exception {
+		
+		// STATION 1
+		client.testInitStation(stationId1, 22, 7, 6, 2);
+		sv1 = client.getInfoStation(stationId1);
+
+		// STATION 2
+		client.testInitStation(stationId2, 80, 20, 12, 1);
+		sv2 = client.getInfoStation(stationId2);
+
+		// STATION 3
+		client.testInitStation(stationId3, 50, 50, 20, 0);
+		sv3 = client.getInfoStation(stationId3);
+
+	}
+
+	@Test
+	public void success1() {
+		CoordinatesView coord = new CoordinatesView();
+		coord.setX(10);
+		coord.setY(10);
+		
+		List<StationView> stations = (List<StationView>) client.listStations(1, coord);
+		
+		assertEquals(1, stations.size());
+		assertEquals(stations.get(0).getId(), sv1.getId());
+		assertEquals(stations.get(0).getCapacity(), sv1.getCapacity());
+		assertEquals(stations.get(0).getAvailableBinas(), sv1.getAvailableBinas());
+		assertEquals(stations.get(0).getFreeDocks(), sv1.getFreeDocks());
+	}
+	
+	@Test
+	public void success2() {
+		CoordinatesView coord = new CoordinatesView();
+		coord.setX(100);
+		coord.setY(20);
+		
+		List<StationView> stations = (List<StationView>) client.listStations(2, coord);
+		
+		assertEquals(2, stations.size());
+		assertEquals(stations.get(0).getId(), sv2.getId());
+		assertEquals(stations.get(0).getCapacity(), sv2.getCapacity());
+		assertEquals(stations.get(0).getAvailableBinas(), sv2.getAvailableBinas());
+		assertEquals(stations.get(0).getFreeDocks(), sv2.getFreeDocks());
+		
+		assertEquals(stations.get(1).getId(), sv3.getId());
+		assertEquals(stations.get(1).getCapacity(), sv3.getCapacity());
+		assertEquals(stations.get(1).getAvailableBinas(), sv3.getAvailableBinas());
+		assertEquals(stations.get(1).getFreeDocks(), sv3.getFreeDocks());
+	}
+	
+	@Test
+	public void success3() {
+		CoordinatesView coord = new CoordinatesView();
+		coord.setX(50);
+		coord.setY(100);
+		
+		List<StationView> stations = (List<StationView>) client.listStations(3, coord);
+		
+		assertEquals(3, stations.size());
+		assertEquals(stations.get(0).getId(), sv3.getId());
+		assertEquals(stations.get(0).getCapacity(), sv3.getCapacity());
+		assertEquals(stations.get(0).getAvailableBinas(), sv3.getAvailableBinas());
+		assertEquals(stations.get(0).getFreeDocks(), sv3.getFreeDocks());
+		
+		assertEquals(stations.get(1).getId(), sv2.getId());
+		assertEquals(stations.get(1).getCapacity(), sv2.getCapacity());
+		assertEquals(stations.get(1).getAvailableBinas(), sv2.getAvailableBinas());
+		assertEquals(stations.get(1).getFreeDocks(), sv2.getFreeDocks());
+		
+		assertEquals(stations.get(2).getId(), sv1.getId());
+		assertEquals(stations.get(2).getCapacity(), sv1.getCapacity());
+		assertEquals(stations.get(2).getAvailableBinas(), sv1.getAvailableBinas());
+		assertEquals(stations.get(2).getFreeDocks(), sv1.getFreeDocks());
+	}
+	
+	@Test
+	public void success4() {
+		CoordinatesView coord = new CoordinatesView();
+		coord.setX(20);
+		coord.setY(10);
+		
+		List<StationView> stations = (List<StationView>) client.listStations(2, coord);
+		
+		assertEquals(2, stations.size());
+		assertEquals(stations.get(0).getId(), sv1.getId());
+		assertEquals(stations.get(0).getCapacity(), sv1.getCapacity());
+		assertEquals(stations.get(0).getAvailableBinas(), sv1.getAvailableBinas());
+		assertEquals(stations.get(0).getFreeDocks(), sv1.getFreeDocks());
+		
+		assertEquals(stations.get(1).getId(), sv3.getId());
+		assertEquals(stations.get(1).getCapacity(), sv3.getCapacity());
+		assertEquals(stations.get(1).getAvailableBinas(), sv3.getAvailableBinas());
+		assertEquals(stations.get(1).getFreeDocks(), sv3.getFreeDocks());
+	}
+		
+	@After
+	public void tearDown() {
+		client.testClear();
+	}
 }
--- binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
+++ binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
@@ -1,7 +1,10 @@
 package org.binas.ws;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.jws.WebService;
 
@@ -15,20 +18,12 @@ import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINamingException;
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDIRecord;
 
-
 /**
  * This class implements the Web Service port type (interface). The annotations
  * below "map" the Java class to the WSDL definitions.
  */
- //TODO
-@WebService(endpointInterface = "org.binas.ws.BinasPortType",
-wsdlLocation = "binas.1_0.wsdl",
-name ="BinasWebService",
-portName = "BinasPort",
-targetNamespace="http://ws.binas.org/",
-serviceName = "BinasService"
-)
-
+// TODO
+@WebService(endpointInterface = "org.binas.ws.BinasPortType", wsdlLocation = "binas.1_0.wsdl", name = "BinasWebService", portName = "BinasPort", targetNamespace = "http://ws.binas.org/", serviceName = "BinasService")
 
 public class BinasPortImpl implements BinasPortType {
 
@@ -46,91 +41,100 @@ public class BinasPortImpl implements BinasPortType {
 	@Override
 	public List<StationView> listStations(Integer numberOfStations, CoordinatesView coordinates) {
 		
+		if (numberOfStations == null || coordinates == null || numberOfStations == 0) {
+			System.out.println("É NULO FILHOS DA PUTA");
+			return null;
+		}
+
 		UDDINaming UDDIname;
 		StationClient s;
-		StationClient closest;
 		Collection<String> stations;
-		Collection<StationClient> ordered = null;
-		List<StationView> result = null;
-		String[] stationsString;
+		List<StationView> result = new ArrayList<StationView>();
+		TreeMap<Integer, StationView> treeResult = new TreeMap<Integer, StationView>();
 		
 		try {
 			UDDIname = this.endpointManager.getUddiNaming();
 			stations = UDDIname.list("A46_Station%");
-			for(int i = 0; i < numberOfStations; i++) {
-				stationsString = (String[]) stations.toArray();
-				closest = new StationClient(stationsString[0]);
-				for(String station : stations) {
-					s = new StationClient(station);	
-					if((Math.pow(s.getInfo().getCoordinate().getX(), 2) + Math.pow(s.getInfo().getCoordinate().getY(), 2)) < (Math.pow(closest.getInfo().getCoordinate().getX(), 2) + Math.pow(closest.getInfo().getCoordinate().getY(), 2)) ){
-						closest = s;
-					}		
-				}
-				stations.remove(closest.getWsURL());
-				result.add(this.getInfoStation(closest.getWsName()));
+			
+			for (String station : stations) {
+				s = new StationClient(station);
+				
+				int distance_X = s.getInfo().getCoordinate().getX() - coordinates.getX();
+				int distance_Y = s.getInfo().getCoordinate().getY() - coordinates.getY();
+				int dist = (int) (Math.pow(distance_X, 2) + Math.pow(distance_Y, 2));
+				
+				StationView d = this.getInfoStation(s.getInfo().getId());
+				treeResult.put(dist, d);
 			}
 			
+			for (int i =0; i < numberOfStations; i++) {
+				Integer firstKey = treeResult.firstKey();
+				result.add(treeResult.get(firstKey));
+				treeResult.remove(firstKey);	
+			}
+			
+			return result;
+			
+			
+			
+
 		} catch (UDDINamingException e) {
 			System.out.println("There was an error while calling UDDINaming at listStations(). Check output: ");
 			e.printStackTrace();
-		}
-		catch (StationClientException e) {
+		} catch (StationClientException e) {
 			System.out.println("There was an error while calling the StationClient at listStations(). Check output: ");
 			e.printStackTrace();
-		}
-		catch (InvalidStation_Exception e) {
+		} catch (InvalidStation_Exception e) {
 			System.out.println("The chosen Station is invalid at listStations(). Check output: ");
 			e.printStackTrace();
 		}
-		
-		return result;
+
+		return null;
+
 	}
 
 	@Override
 	public StationView getInfoStation(String stationId) throws InvalidStation_Exception {
-		
+
 		System.out.println("Station id:" + stationId);
-		
-		if (stationId == null || stationId == "" || !stationId.startsWith("A46_Station")) throw new InvalidStation_Exception(stationId, null);
-		
-		
+
+		if (stationId == null || stationId == "" || !stationId.startsWith("A46_Station"))
+			throw new InvalidStation_Exception(stationId, null);
+
 		UDDINaming UDDIname;
 		StationClient s;
 		org.binas.station.ws.StationView svS = new org.binas.station.ws.StationView();
 		StationView svB = new StationView();
-	
-		
+
 		try {
-			
+
 			UDDIname = this.endpointManager.getUddiNaming();
 			String url = UDDIname.lookup(stationId);
 			s = new StationClient(url);
 			svS = s.getInfo();
-			
-			System.out.println("Station id:" + svS.getId());
-			
+
+
 		} catch (StationClientException e) {
 			throw new InvalidStation_Exception(stationId, null);
 		} catch (UDDINamingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
 
-			CoordinatesView coord = new CoordinatesView();
-			coord.setX(svS.getCoordinate().getX());
-			coord.setY(svS.getCoordinate().getY());
-			
-			svB.setAvailableBinas(svS.getAvailableBinas());
-			svB.setCapacity(svS.getCapacity());
-			svB.setCoordinate(coord);
-			svB.setFreeDocks(svS.getFreeDocks());
-			svB.setId(svS.getId());
-			svB.setTotalGets(svS.getTotalGets());
-			svB.setTotalReturns(svS.getTotalReturns());
-			
-			return svB;
-		
+		CoordinatesView coord = new CoordinatesView();
+		coord.setX(svS.getCoordinate().getX());
+		coord.setY(svS.getCoordinate().getY());
+
+		svB.setAvailableBinas(svS.getAvailableBinas());
+		svB.setCapacity(svS.getCapacity());
+		svB.setCoordinate(coord);
+		svB.setFreeDocks(svS.getFreeDocks());
+		svB.setId(svS.getId());
+		svB.setTotalGets(svS.getTotalGets());
+		svB.setTotalReturns(svS.getTotalReturns());
+
+		return svB;
+
 	}
 
 	@Override
@@ -139,7 +143,7 @@ public class BinasPortImpl implements BinasPortType {
 		user = User.getUser(email);
 		System.out.println("ESTOU NO BINASIMPL: " + user.getEmail());
 		int credit = user.getCredit();
-		
+
 		return credit;
 	}
 
@@ -153,19 +157,24 @@ public class BinasPortImpl implements BinasPortType {
 	@Override
 	public void rentBina(String stationId, String email) throws AlreadyHasBina_Exception, InvalidStation_Exception,
 			NoBinaAvail_Exception, NoCredit_Exception, UserNotExists_Exception {
-		if (stationId == null || stationId.trim().equals("") || !stationId.startsWith("A46_Station")) throw new InvalidStation_Exception(email, null);
-		if (email == null || email == "") throw new UserNotExists_Exception(email, null);
+		if (stationId == null || stationId.trim().equals("") || !stationId.startsWith("A46_Station"))
+			throw new InvalidStation_Exception(email, null);
+		if (email == null || email == "")
+			throw new UserNotExists_Exception(email, null);
 		StationClient s;
 		User user;
 		try {
 			user = User.getUser(email);
 			s = new StationClient(this.endpointManager.getUddiUrl(), stationId);
 			org.binas.station.ws.StationView sv = s.getInfo();
-			
-			if (user.isHasBina()) throw new AlreadyHasBina_Exception("User has already rented a bina.", null);
-			if (sv.getAvailableBinas() == 0) throw new NoBinaAvail_Exception("There is no bina available at this station.", null);
-			if (user.getCredit() <= 0) throw new NoCredit_Exception("User does not have enought credits to rent the bina.", null);
-			
+
+			if (user.isHasBina())
+				throw new AlreadyHasBina_Exception("User has already rented a bina.", null);
+			if (sv.getAvailableBinas() == 0)
+				throw new NoBinaAvail_Exception("There is no bina available at this station.", null);
+			if (user.getCredit() <= 0)
+				throw new NoCredit_Exception("User does not have enought credits to rent the bina.", null);
+
 			else {
 				s.getBina();
 				user.setHasBina(true);
@@ -175,48 +184,42 @@ public class BinasPortImpl implements BinasPortType {
 			throw new InvalidStation_Exception(email, null);
 		} catch (org.binas.station.ws.NoBinaAvail_Exception e) {
 			throw new NoBinaAvail_Exception("There is no bina available at this station.", null);
-		} 
+		}
 	}
 
 	@Override
 	public void returnBina(String stationId, String email)
 			throws FullStation_Exception, InvalidStation_Exception, NoBinaRented_Exception, UserNotExists_Exception {
-		
-		if (stationId == null || stationId.trim().equals("") || !stationId.startsWith("A46_Station")) throw new InvalidStation_Exception(email, null);
-		
+
+		if (stationId == null || stationId.trim().equals("") || !stationId.startsWith("A46_Station"))
+			throw new InvalidStation_Exception(email, null);
+
 		StationClient s;
 		User user;
 		try {
 			s = new StationClient(this.endpointManager.getUddiUrl(), stationId);
 			org.binas.station.ws.StationView sv = s.getInfo();
-			
-			
+
 			user = User.getUser(email);
 			UserView userView = user.getUserView();
-		
-			
+
 			if (user.isHasBina()) {
 				if (sv.getFreeDocks() == 0) {
 					throw new FullStation_Exception("This station is full", null);
-				}
-				else{
+				} else {
 					int bonus = s.returnBina();
 					user.setHasBina(false);
 					user.addCredit(bonus);
 				}
-			}
-			else throw new NoBinaRented_Exception(email, null);
-			
+			} else
+				throw new NoBinaRented_Exception(email, null);
+
 		} catch (StationClientException e) {
 			System.out.println("There was an error while calling the StationClient. Check output: " + e);
 		} catch (NoSlotAvail_Exception e) {
 			System.out.println("There is no slot available at this station.");
-		} 
-		
-		
-		
-		
-		
+		}
+
 	}
 
 	@Override
@@ -224,93 +227,88 @@ public class BinasPortImpl implements BinasPortType {
 		UDDINaming UDDIname;
 		String res = "";
 		Collection<UDDIRecord> stations;
-		if (inputMessage == null || inputMessage.trim().equals("")) return null;
-		
+		if (inputMessage == null || inputMessage.trim().equals(""))
+			return null;
+
 		try {
 			UDDIname = this.endpointManager.getUddiNaming();
 			stations = UDDIname.listRecords("A46_Station%");
-			
+
 			res += "Found " + stations.size() + " stations.\n";
-			
+
 			for (UDDIRecord stationName : stations) {
 				System.out.println(stationName);
 			}
-			
+
 			for (UDDIRecord stationName : stations) {
 				res += "[Pinging Station" + stationName.getOrgName() + "][Aswser]";
 				StationClient sc = new StationClient(stationName.getUrl());
 				res += sc.testPing(inputMessage) + "\n";
 			}
 			System.out.println(res);
-		}
-		catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return res;
-		
+
 	}
 
 	@Override
 	public void testClear() {
-			User.clear();
-			Collection<UDDIRecord> stations = null;
-			
-			UDDINaming UDDIname;
+		User.clear();
+		Collection<UDDIRecord> stations = null;
+
+		UDDINaming UDDIname;
+		try {
+			UDDIname = this.endpointManager.getUddiNaming();
+			stations = UDDIname.listRecords("A46_Station%");
+		} catch (UDDINamingException e1) {
+			System.out.println("There was an error while calling UDDINaming at listStations(). Check output: ");
+			e1.printStackTrace();
+		}
+		StationClient sc;
+		for (UDDIRecord stationName : stations) {
 			try {
-				UDDIname = this.endpointManager.getUddiNaming();
-				stations = UDDIname.listRecords("A46_Station%");
-			} catch (UDDINamingException e1) {
-				System.out.println("There was an error while calling UDDINaming at listStations(). Check output: ");
-				e1.printStackTrace();
+				sc = new StationClient(stationName.getUrl());
+			} catch (StationClientException e) {
+				continue;
 			}
-			StationClient sc;
-			for (UDDIRecord stationName : stations) {
-				try {
-					sc = new StationClient(stationName.getUrl());
-				} catch (StationClientException e) {
-					continue;
-				}
-				
-				sc.setWsName(stationName.getOrgName());
-				sc.testClear();
-				
-			}
-			
-		
-		
+
+			sc.setWsName(stationName.getOrgName());
+			sc.testClear();
+
+		}
+
 	}
 
 	@Override
 	public void testInitStation(String stationId, int x, int y, int capacity, int returnPrize)
 			throws BadInit_Exception {
-		
+
 		String UDDIname;
 		StationClient sc;
-		
+
 		try {
 			UDDIname = this.endpointManager.getUddiUrl();
 			System.out.println("Station id no testInitStation antes: " + stationId);
 			sc = new StationClient(UDDIname, stationId);
 			System.out.println("Station id no testInitStation depois: " + sc.getInfo().getId());
 			sc.testInit(x, y, capacity, returnPrize);
-			
-		}catch (StationClientException e) {
-			System.out.println("There was an error while calling the StationClient at testInitStation(). Check output: ");
+
+		} catch (StationClientException e) {
+			System.out
+					.println("There was an error while calling the StationClient at testInitStation(). Check output: ");
 			e.printStackTrace();
-		}
-		catch (org.binas.station.ws.BadInit_Exception e) {
+		} catch (org.binas.station.ws.BadInit_Exception e) {
 			System.out.println("There was an error while trying to Init at testInitStation(). Check output: ");
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 	@Override
 	public void testInit(int userInitialPoints) throws BadInit_Exception {
 		User.setDef(userInitialPoints);
 	}
-	
-
 
 }
