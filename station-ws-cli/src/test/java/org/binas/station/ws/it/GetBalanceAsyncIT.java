48fee9d0c40bd4fe7dbdad953c156a059c8136d7
heliodomingos <helio_domingos@hotmail.com>
2018-05-02 15:37:11 +0100

    #funciona adicionado GetBalanceAsyncIT e SetBalanceIT

---
+++ station-ws-cli/src/test/java/org/binas/station/ws/it/GetBalanceAsyncIT.java
@@ -0,0 +1,58 @@
+package org.binas.station.ws.it;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.ws.Response;
+
+import org.binas.station.ws.BadInit_Exception;
+import org.binas.station.ws.GetBalanceResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GetBalanceAsyncIT extends BaseIT{
+	private final static int X = 5;
+	private final static int Y = 5;
+	private final static int CAPACITY = 20;
+	private final static int RETURN_PRIZE = 0;
+	
+	// one-time initialization and clean-up
+	@BeforeClass
+	public static void oneTimeSetUp() {
+	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+	}
+
+	// members
+
+	// initialization and clean-up for each test
+	@Before
+	public void setUp() throws BadInit_Exception {
+		client.testClear();
+		client.testInit(X, Y, CAPACITY, RETURN_PRIZE);
+		client.setBalanceAsync("email@valido", 2, 1);
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	// main tests
+
+	@Test
+	public void success() throws InterruptedException, ExecutionException{
+		Response<GetBalanceResponse> response = client.getBalanceAsync("email@valido");
+		int tag = response.get().getBalanceView().getTag();
+		int value = response.get().getBalanceView().getValue();
+		assertEquals(1, tag);
+		assertEquals(2, value);
+	}
+
+
+}
